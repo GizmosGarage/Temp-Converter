37cc162b041d30d70a30e5b25bc32d230289cd47
Ethan Sessoms <ethan.e.sessoms@gmail.com>
2026-09-21 22:28:06 -0400

    Figures out which unit and correctly converts

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-## Temperature Converter
+# Temperature Converter
 
 I am learning how to program with Python.
 I am also trying to learn through creating, no lessons or classes.
 
-# What I'm practicing
+## What I'm practicing
 
 - Functions
 - User input
 
-# Run
+## Run
 
 python3 Temp_Convert.py
--- Temp_Convert.py
+++ Temp_Convert.py
@@ -1,9 +1,22 @@
-# Ask the user for an input
-temp = float(input("Enter Fahrenheit: "))
+# Ask the user for a temperature
+temp = input("Enter Temperature: ")
+
+# Convert Celsius to Fahrenheit
+def cels_to_fahr(tempc):
+    fahr = (9 / 5) * (tempc) + 32
+    print(round(fahr))
 
 # Convert Fahrenheit to Celsius
-def fahr_to_cels(temp):
-    cels = (temp - 32) * (5/9)
-    return round(cels)
+def fahr_to_cels(tempf):
+    cels = (tempf - 32) * (5 / 9)
+    print(round(cels))
 
-print(fahr_to_cels(temp))
+# Figure out if Fahrenheit or Celsius
+if temp[-1] == "f":
+    tempf = float(temp[:-1])
+    fahr_to_cels(tempf)
+elif temp[-1] == "c":
+    tempc = float(temp[:-1])
+    cels_to_fahr(tempc)
+else:
+    print("Please end input with either 'f' or 'c'")

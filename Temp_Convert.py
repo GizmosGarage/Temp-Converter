0035ac18e361b4afccdef6796e9e9439763390b0
GizmosGarage <210449727+GizmosGarage@users.noreply.github.com>
2026-09-23 16:29:35 -0400

    Infinitly replayable until user enters 'quit'.

--- Temp_Convert.py
+++ Temp_Convert.py
@@ -1,6 +1,3 @@
-# Ask the user for a temperature
-temp = input("Enter Temperature: ")
-
 # Convert Celsius to Fahrenheit
 def cels_to_fahr(tempc):
     fahr = (9 / 5) * (tempc) + 32
@@ -11,12 +8,19 @@ def fahr_to_cels(tempf):
     cels = (tempf - 32) * (5 / 9)
     print(f"It is {round(cels)} Celsisus")
 
-# Figure out if Fahrenheit or Celsius
-if temp[-1] == "f":
-    tempf = float(temp[:-1])
-    fahr_to_cels(tempf)
-elif temp[-1] == "c":
-    tempc = float(temp[:-1])
-    cels_to_fahr(tempc)
-else:
-    print("Please end input with either 'f' or 'c'")
+# Keep asking for temperatures until the user wants to quit
+while True:
+    temp = input("Enter Temperature (or 'quit' to exit): ")
+
+    if temp == "quit":
+        break
+
+    # Figure out if Fahrenheit or Celsius
+    if temp[-1] == "f":
+        tempf = float(temp[:-1])
+        fahr_to_cels(tempf)
+    elif temp[-1] == "c":
+        tempc = float(temp[:-1])
+        cels_to_fahr(tempc)
+    else:
+        print("Please end input with either 'f' or 'c'")
